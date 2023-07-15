63cb81d52929df4cb542c2b3b25f559f110c5e40
Chebupela <aristarhsakulin@gmail.com>
2023-07-15 21:31:58 +0300

    Initial Commit

---
+++ hz/hzApp.swift
@@ -0,0 +1,17 @@
+//
+//  hzApp.swift
+//  hz
+//
+//  Created by aristarh on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct hzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
